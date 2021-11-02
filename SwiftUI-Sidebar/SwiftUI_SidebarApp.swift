c56232dabde9bd752ace69a096f833c99160d667
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-02 13:11:39 -0500

    Initial Commit

---
+++ SwiftUI-Sidebar/SwiftUI_SidebarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SidebarApp.swift
+//  SwiftUI-Sidebar
+//
+//  Created by R. Mark Volkmann on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SidebarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
